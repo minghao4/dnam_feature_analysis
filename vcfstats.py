51765abe592f246f8a4362f8b4a8b9d32a243aab
minghao4 <minghao.li1@ucalgary.ca>
2018-10-04 16:22:38 -0600

    multiprocess implementation for phenotype_regressor

--- vcfstats.py
+++ vcfstats.py
@@ -5,10 +5,10 @@ import sys
 # import vcfstats.vcf_parser as vp
 # import vcfstats.sequence_divergence as sd
 # import vcfstats.pi_regression as pr
-import vcfstats.methylation_binner as mb
+# import vcfstats.methylation_binner as mb
 # from vcfstats import global_t_test as gtt
 # from vcfstats import cultivar_t_tester as ctt
-# from vcfstats import phenotype_regressor as pmr
+from vcfstats import phenotype_regressor as pmr
 
 # input_path = sys.argv[1]
 # parser = vp.VcfParser()
@@ -45,12 +45,12 @@ import vcfstats.methylation_binner as mb
 
 # )
 
-bin_file_path = sys.argv[1]
-methylation_file_path = sys.argv[2]
-output_dir_path = sys.argv[3]
+# bin_file_path = sys.argv[1]
+# methylation_file_path = sys.argv[2]
+# output_dir_path = sys.argv[3]
 
-m_bin = mb.MethylationBinner()
-m_bin.calculate_all_bin_methylation(bin_file_path, methylation_file_path, output_dir_path)
+# m_bin = mb.MethylationBinner()
+# m_bin.calculate_all_bin_methylation(bin_file_path, methylation_file_path, output_dir_path)
 
 
 # lethbridge_methLvl_file = sys.argv[1]
@@ -67,11 +67,10 @@ m_bin.calculate_all_bin_methylation(bin_file_path, methylation_file_path, output
 # c_tt = ctt.PairedTTester()
 # c_tt.paired_t_tests(lethbridge_file_path, vegreville_file_path, output_dir_path)
 
-# delta_phenotype_file_path = sys.argv[1]
-# delta_methylation_file_path = sys.argv[2]
-# output_dir_path = sys.argv[3]
+delta_phenotype_file_path = sys.argv[1]
+delta_methylation_file_path = sys.argv[2]
+output_dir_path = sys.argv[3]
 
-# pm_rg = pmr.PhenotypeRegressor()
-# pm_rg.phenotype_methylation_regression(
-#     delta_phenotype_file_path, delta_methylation_file_path, output_dir_path
-# )
+pmr.phenotype_methylation_regression(
+    delta_phenotype_file_path, delta_methylation_file_path, output_dir_path
+)
--- vcfstats/phenotype_regressor.py
+++ vcfstats/phenotype_regressor.py
@@ -7,7 +7,9 @@
 from .__init__ import timeit, df, pd, sps
 from . import helpers
 
-# import sys
+import multiprocessing
+import sys
+import warnings
 
 import statsmodels.formula.api as smf
 
@@ -18,15 +20,13 @@ start_time = timeit.default_timer()
 # output_dir_path = sys.argv[3]
 
 
-class PhenotypeRegressor:
+class PhenotypeRegressionInput:
     def __init__(self):
         self.phenotype_df = None
         self.methylation_df = None
-        self.current_bin_df = None
-        self.current_output_df = None
 
 
-    def __set_dfs(
+    def set_input_dfs(
             self, delta_phenotype_file_path: str,
             delta_methylation_file_path: str
         ) -> None:
@@ -36,110 +36,165 @@ class PhenotypeRegressor:
             delta_phenotype_file_path, index_col = 0
         )
 
-        cols = ["#Scaffold", "Bin_Label"].append(
-            self.phenotype_df.index.tolist()
-        )
+        cols = ["#Scaffold", "Bin_Label"] + self.phenotype_df.index.tolist()
         self.methylation_df = pd.read_table(
             delta_methylation_file_path, usecols = cols
         )
 
+
+class PhenotypeRegressionOutput:
+    def __init__(self):
+        self.current_bin_df = None
+        self.phenotype_output_df = None
+
+
+    def __set_output_dfs(
+            self, methylation_input_df: pd.DataFrame
+        ) -> None:
+        """
+        """
         self.current_bin_df = df(
-            data = 0, index = self.phenotype_df.index,
+            data = 0, index = methylation_input_df.columns[2:],
             columns = ["delta_phenotype", "delta_methylation"]
         )
 
-        self.current_output_df = self.methylation_df.iloc[:, 0:2]
-        self.current_output_df["R_Squared"] = 0
-        self.current_output_df["P_Value"] = 0
-        self.current_output_df["Significant?"] = False
+        self.phenotype_output_df = methylation_input_df.iloc[:, 0:2]
+        self.phenotype_output_df["R_Squared"] = 0
+        self.phenotype_output_df["P_Value"] = 0
+        self.phenotype_output_df["Significant?"] = False
 
 
-    def __bin_regression(self, phenotype_data: pd.Series) -> None:
+    def __bin_regression(
+            self, phenotype_data: pd.Series, methylation_df: pd.DataFrame
+        ) -> None:
         """
         """
         phenotype_label = phenotype_data.name
         self.current_bin_df["delta_phenotype"] = phenotype_data
 
-        for bin_idx in range(self.methylation_df.shape[0]):
-            bin_row = self.methylation_df.loc[bin_idx]
+        for bin_idx in range(methylation_df.shape[0]):
+            bin_row = methylation_df.loc[bin_idx]
             quick_search = helpers.string_builder((
-                phenotype_label, "-", bin_row[0], "-", str(bin_row[1])
+                phenotype_label, '-', bin_row[0], '-', str(bin_row[1])
             ))
-            self.current_bin_df["delta_methylation"] = bin_row[2:]
 
-            model = smf.ols(
-                "delta_phenotype ~ delta_methylation",
-                data = self.current_bin_df
-            ).fit()
-            self.current_output_df.iloc[bin_idx, 2] = model.rsquared
-            self.current_output_df.iloc[bin_idx, 3] = 0
-            self.current_output_df.iloc[bin_idx, 4] = False
-            try:
-                self.current_output_df.iloc[bin_idx, 3] = model.pvalues[1]
-            except:
-                pass
+            methylation_data = pd.to_numeric(bin_row[2:])
+            if methylation_data.nonzero()[0].size != 0:
+                self.current_bin_df["delta_methylation"] = methylation_data
+                warnings.filterwarnings("ignore")
+                model = smf.ols(
+                    "delta_phenotype ~ delta_methylation",
+                    data = self.current_bin_df
+                ).fit()
 
-            try:
-                self.current_output_df.iloc[bin_idx, 4] = \
-                    helpers.significance(model.pvalues.tolist())
-            except:
-                pass
+                self.phenotype_output_df.iloc[bin_idx, 2] = model.rsquared
+                self.phenotype_output_df.iloc[bin_idx, 3] = 0
+                self.phenotype_output_df.iloc[bin_idx, 4] = False
+                try:
+                    self.phenotype_output_df.iloc[bin_idx, 3] = model.pvalues[1]
+                except:
+                    pass
 
-            print()
-            print("++++++++++")
-            print(quick_search)
-            print(model.summary())
-            print("++++++++++")
-            print()
+                try:
+                    self.phenotype_output_df.iloc[bin_idx, 4] = \
+                        helpers.significance(model.pvalues.tolist())
+                except:
+                    pass
 
+                print(
+                    helpers.string_builder((
+                        '\n', "++++++++++", '\n', quick_search, '\n'
+                    ))
+                )
+                print(model.summary())
+                print(helpers.string_builder(('\n', "++++++++++", '\n')))
 
-    def __phenotype_regression(self, output_dir_path:str) -> None:
-        """
-        """
-        for phenotype in self.phenotype_df.columns.tolist():
-            print()
-            print("-----**********-----")
-            print(helpers.string_builder(("Phenotype: ", phenotype)))
-            print("-----**********-----")
-            print()
-
-            self.__bin_regression(self.phenotype_df[phenotype])
-
-            helpers.write_output(
-                self.current_output_df,
-                helpers.string_builder((
-                    phenotype, "_", "phenotype_regression.tsv"
-                )), output_dir_path
-            )
+            else:
+                print(
+                    helpers.string_builder((
+                        '\n', bin_row[0], '-', str(bin_row[1]),
+                        " has been filtered for ", phenotype_label, "...", '\n'
+                    ))
+                )
 
 
-    def phenotype_methylation_regression(
-            self, delta_phenotype_file_path: str,
-            delta_methylation_file_path: str, output_dir_path: str
+    def phenotype_regression(
+            self, phenotype_data: pd.Series, methylation_input_df: pd.DataFrame,
+            output_dir_path: str
         ) -> None:
         """
         """
-        print()
-        print("Start")
-        helpers.remove_trailing_slash((
-            delta_phenotype_file_path, delta_methylation_file_path,
-            output_dir_path
-        ))
-
-        print()
-        print("Setting input dataframes...")
-        self.__set_dfs(delta_phenotype_file_path, delta_methylation_file_path)
-
-        print()
-        print("Performing phenotype regression...")
-        self.__phenotype_regression(output_dir_path)
-
-        helpers.print_program_runtime(
-            "Phenotype regression analyses", start_time
+        phenotype = phenotype_data.name
+        sys.stdout = open(
+            helpers.string_builder((phenotype, "_stdout.txt")), 'w'
+        )
+        print(
+            helpers.string_builder((
+                '\n', "-----**********-----", '\n', "Phenotype: ",
+                phenotype, '\n', "-----**********-----", '\n'
+            ))
         )
 
+        self.__set_output_dfs(methylation_input_df)
+        self.__bin_regression(
+            phenotype_data, methylation_input_df
+        )
 
-# pmr = PhenotypeRegressor()
-# pmr.phenotype_methylation_regression(
-#     delta_phenotype_file_path, delta_methylation_file_path, output_dir_path
+        helpers.write_output(
+            self.phenotype_output_df,
+            helpers.string_builder((
+                phenotype, '_', "phenotype_regression.tsv"
+            )), output_dir_path
+        )
+        sys.stdout.close()
+
+
+def phenotype_methylation_regression(
+        delta_phenotype_file_path: str, delta_methylation_file_path: str,
+        output_dir_path: str
+    ) -> None:
+    """
+    Main method.
+    """
+    print(helpers.string_builder(('\n', "Start.")))
+    helpers.remove_trailing_slash([
+        delta_phenotype_file_path, delta_methylation_file_path,
+        output_dir_path
+    ])
+
+    print(helpers.string_builder(('\n', "Setting dataframes...")))
+    inputs = PhenotypeRegressionInput()
+    inputs.set_input_dfs(delta_phenotype_file_path, delta_methylation_file_path)
+
+    print(helpers.string_builder(('\n', "Performing phenotype regression...")))
+    # output = PhenotypeRegressionOutput()
+    # output.phenotype_regression(
+    #     inputs.phenotype_df["height"], inputs.methylation_df, output_dir_path,
+    #     start_time
+    # )
+    outputs = []
+    for phenotype in inputs.phenotype_df.columns.tolist():
+        output = PhenotypeRegressionOutput()
+        process = multiprocessing.Process(
+            target = output.phenotype_regression,
+            args = (
+                inputs.phenotype_df[phenotype], inputs.methylation_df,
+                output_dir_path
+            )
+        )
+        outputs.append(process)
+
+    for process in outputs:
+        process.start()
+
+    for process in outputs:
+        process.join()
+
+    helpers.print_program_runtime(
+        "Phenotype regression analyses", start_time
+    )
+
+
+# phenotype_methylation_regression(
+#     delta_methylation_file_path, delta_methylation_file_path, output_dir_path
 # )
