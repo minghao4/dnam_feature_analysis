3685925be107ee63c28b5eadbcd646c33486b574
minghao4 <minghao.li1@ucalgary.ca>
2018-11-13 13:11:17 -0700

    repo naming restructuring

--- dnam_feature_analysis/__init__.py
+++ dnam_feature_analysis/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Module imports.
+VCF Stats
 
 """
 
---
+++ dnam_feature_analysis/user_interface.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+User interface.
+
+"""
+
+import vcfstats
+
+
+
+
